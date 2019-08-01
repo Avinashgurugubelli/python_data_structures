779f7c0a9c82d4c306fb621d05cbc2eb26b42d9c
avinash <avinashgurugubelli@gmail.com>
2019-08-02 00:33:20 +0530

    code with error added in tree folder

--- trees/binary_tree/main.py
+++ trees/binary_tree/main.py
@@ -4,8 +4,12 @@ CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(CURRENT_DIR))
 
 from binary_tree_node import BinaryTreeNode
+from binary_tree import BinaryTree
+from .utils.binary_tree_traversal_types import BinaryTreeTraversalMethodType, BinaryTreeTraversalType
 
 if __name__ == "__main__":
     node1 = BinaryTreeNode(1)
     node1.left = BinaryTreeNode(2)
     node1.right = BinaryTreeNode(3)
+    binaryTree = BinaryTree()
+    binaryTree.traverse(BinaryTreeTraversalType.PRE_ORDER, BinaryTreeTraversalMethodType.ITERATIVE, node1)
